86dffbc68684c2b95d9a0d973a5f1b7ffd274606
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-01 12:51:09 +0200

    add node to scene

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
+                                       ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0.3,0.2,-0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
     }
 
 
