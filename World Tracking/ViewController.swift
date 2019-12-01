de70cc16c2d6b54e597639dbc93a631755245747
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-01 13:27:48 +0200

    light and random nodes

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    var counter = 0.0
     let configuration = ARWorldTrackingConfiguration()
     
    
@@ -20,16 +21,41 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
                                        ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/4)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0.3,0.2,-0.3)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x,y,z)
         self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession(){
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes{(node,_) in
+            node.removeFromParentNode()
+        }
+        
+        self.sceneView.session.run(configuration, options: [
+            .resetTracking,.removeExistingAnchors
+        ])
+    }
+    
+    func randomNumbers(firstNum:CGFloat,secondNum:CGFloat) -> CGFloat {
+        return CGFloat(arc4random())/CGFloat(UINT32_MAX) * abs(firstNum - secondNum) +
+        min(firstNum,secondNum)
     }
 
 
