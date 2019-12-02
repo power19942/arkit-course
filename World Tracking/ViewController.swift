97f21e4585e73384327712ee7d61da4f061c5816
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-02 14:17:38 +0200

    add moon to earth

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -22,22 +22,45 @@ class ViewController: UIViewController {
     
     
     override func viewDidAppear(_ animated: Bool) {
-
-        let earth = planet(geometry: SCNSphere(radius: 0.2), position: SCNVector3(1.0,0,0),diffuse: "Earth day", specular: "Earth Specular", emission: "Earth Emission", normal: "Earth Normal")
         
-        let venus = planet(geometry: SCNSphere(radius: 0.15), position: SCNVector3(-1.2,0,0),diffuse: "Venus Atmosphere", specular: "", emission: "Venus Serface", normal: "String")
+        let earthParent = SCNNode()
+        let venusParent = SCNNode()
+        
+        earthParent.position = SCNVector3(0,0,-1)
+        venusParent.position = SCNVector3(0,0,-1)
+
+        let earth = planet(geometry: SCNSphere(radius: 0.2), position: SCNVector3(0.9,0,0),diffuse: "Earth day", specular: "Earth Specular", emission: "Earth Emission", normal: "Earth Normal")
+        
+        let moon = planet(geometry: SCNSphere(radius: 0.05), position: SCNVector3(0.5,0,0),diffuse: "Moon", specular: "", emission: "", normal: "")
+        
+        let venus = planet(geometry: SCNSphere(radius: 0.15), position: SCNVector3(-0.9,0,0),diffuse: "Venus Atmosphere", specular: "", emission: "Venus Serface", normal: "String")
         
         let sun = planet(geometry: SCNSphere(radius: 0.35), position: SCNVector3(0,0,-1),diffuse: "Sun diffuse", specular: "Sun Atmosphere", emission: "String", normal: "String")
         //self.sceneView.scene.rootNode.addChildNode(earth)
+        
         self.sceneView.scene.rootNode.addChildNode(sun)
-        sun.addChildNode(earth)
-        sun.addChildNode(venus)
+        self.sceneView.scene.rootNode.addChildNode(earthParent)
+        self.sceneView.scene.rootNode.addChildNode(venusParent)
+        
+        earth.addChildNode(moon)
+        earthParent.addChildNode(earth)
+        venusParent.addChildNode(venus)
         self.sceneView.autoenablesDefaultLighting = true
-        let action = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 8)
-        let forever = SCNAction.repeatForever(action)
-        earth.runAction(forever)
-        venus.runAction(forever)
-        sun.runAction(forever)
+        let sunAction = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 5)
+        let sunForever = SCNAction.repeatForever(sunAction)
+        
+        let earthAction = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 10)
+        let earthForever = SCNAction.repeatForever(earthAction)
+        
+        let venusAction = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 5)
+        let venusForever = SCNAction.repeatForever(venusAction)
+        
+        moon.runAction(earthForever)
+        earth.runAction(earthForever)
+        venus.runAction(venusForever)
+        earthParent.runAction(earthForever)
+        venusParent.runAction(venusForever)
+        sun.runAction(sunForever)
     }
     
     func planet(geometry:SCNGeometry,position:SCNVector3,diffuse:String,specular:String,emission:String,normal:String) -> SCNNode {
