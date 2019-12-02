c4a94384522782764f63d1aab11abeedecac45d4
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-02 11:41:35 +0200

    3d drawing

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -9,14 +9,60 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , ARSCNViewDelegate{
+    let config = ARWorldTrackingConfiguration()
     
-   
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    @IBOutlet weak var draw: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [
+            ARSCNDebugOptions.showFeaturePoints,
+            ARSCNDebugOptions.showWorldOrigin
+        ]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(config)
+        self.sceneView.delegate = self
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        guard let pointOfView = sceneView.pointOfView else {return}
+        
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let currentPositionOfCamera = orientation + location
+        DispatchQueue.main.async{
+            if self.draw.isHighlighted {
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = currentPositionOfCamera
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+                self.sceneView.scene.rootNode.addChildNode(sphereNode)
+            }else{
+                let pointer = SCNNode(geometry: SCNSphere(radius: 0.01))
+                pointer.name = "pointer"
+                pointer.position = currentPositionOfCamera
+                pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+                self.sceneView.scene.rootNode.enumerateChildNodes({(node,_) in
+                    if node.name == "pointer" {
+                        node.removeFromParentNode()
+                    }
+                })
+                self.sceneView.scene.rootNode.addChildNode(pointer)
+            }
+        }
+        
     }
     
 
+    
+}
+
+func + (left:SCNVector3,right:SCNVector3)-> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
 }
 
 
