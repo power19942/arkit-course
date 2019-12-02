658710dd37bbb717c69113b9ac96008ae7fcc1b5
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-02 12:42:34 +0200

    create planet earth

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -12,11 +12,33 @@ import ARKit
 class ViewController: UIViewController {
     
    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let config = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(config)
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Earth day")
+        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "Earth Specular")
+        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "Earth Emission")
+        earth.geometry?.firstMaterial?.normal.contents = UIImage(named: "Earth Normal")
+        earth.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        self.sceneView.autoenablesDefaultLighting = true
+        let action = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
+    }
 
 }
 
 
+extension Int {
+    var degreesToRadians: CGFloat { return CGFloat(Double(self) * .pi/180)}
+}
