587e932cdab8b97943a556a08ecb68beef134e20
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-03 10:45:16 +0200

    add jellyfish model

---
+++ World Tracking/MyController.swift
@@ -0,0 +1,30 @@
+//
+//  MyController.swift
+//  World Tracking
+//
+//  Created by opstudios on 12/3/19.
+//  Copyright © 2019 opstudios. All rights reserved.
+//
+
+import UIKit
+
+class MyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -12,11 +12,53 @@ import ARKit
 class ViewController: UIViewController {
     
    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let config = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView.debugOptions = [
+            ARSCNDebugOptions.showWorldOrigin,
+            ARSCNDebugOptions.showFeaturePoints
+        ]
+        sceneView.session.run(config)
+        let tabGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tabGestureRecognizer)
     }
     
-
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        
+    }
+    
+    func addNode(){
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "jf", recursively: false)
+        jellyFishNode?.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+//        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        node.position = SCNVector3(0,0,-1)
+//        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @objc func handleTap(sender:UITapGestureRecognizer){
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didnt touch anything")
+        }else{
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
+        
+    }
+    
 }
 
 
