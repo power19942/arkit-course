b00c419259f6f4e2f2d80377b1ce14265c1524c7
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-03 12:24:12 +0200

    lava app

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , ARSCNViewDelegate{
     
    
     @IBOutlet weak var scene: ARSCNView!
@@ -20,10 +20,57 @@ class ViewController: UIViewController {
             ARSCNDebugOptions.showFeaturePoints,
             ARSCNDebugOptions.showWorldOrigin
         ]
+        self.config.planeDetection = .horizontal
         self.scene.session.run(config)
+        self.scene.delegate = self
+
+    }
+    
+    func createLava(planeAnchor:ARPlaneAnchor)-> SCNNode{
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians,0,0)
+        return lavaNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planAnchore = anchor as? ARPlaneAnchor else {
+            return
+        }
+        let lavaNode = createLava(planeAnchor: planAnchore)
+        node.addChildNode(lavaNode)
+        scene.scene.rootNode.addChildNode(lavaNode)
+        print("new flat surface detected")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planAnchore = anchor as? ARPlaneAnchor else {
+            return
+        }
+        node.enumerateChildNodes{(childNode,_) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planAnchore)
+        node.addChildNode(lavaNode)
+        print("update")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        node.enumerateChildNodes{(childNode,_) in
+            childNode.removeFromParentNode()
+        }
+        print("remove")
     }
     
 
+}
+
+extension Int {
+    var degreesToRadians: Double {
+        return Double(self) * .pi/180
+    }
 }
 
 
