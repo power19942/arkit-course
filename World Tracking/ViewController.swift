27b03fb70eee3cee4948220f1ebbb15a6a468f60
omar jakmira <o.jaqmira@nano-studios.com>
2019-12-09 12:33:09 +0200

    complete the game

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -8,131 +8,109 @@
 
 import UIKit
 import ARKit
-import Each
 
-class ViewController: UIViewController,ARSCNViewDelegate {
+enum BitMaskCategory : Int {
+    case bullet = 2
+    case target = 3
+}
+
+class ViewController: UIViewController,ARSCNViewDelegate,SCNPhysicsContactDelegate {
     
     
     @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     let config = ARWorldTrackingConfiguration()
-    var power:Float = 1.0
-    var timer = Each(0.05).seconds
-    var basketAdded :Bool = false
+    var power:Float = 20
+    var Target:SCNNode?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [
             ARSCNDebugOptions.showFeaturePoints,
             ARSCNDebugOptions.showWorldOrigin
         ]
-        self.config.planeDetection = .horizontal
         self.sceneView.autoenablesDefaultLighting = true
         self.sceneView.session.run(config)
         self.sceneView.delegate = self
         let tapRec = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         self.sceneView.addGestureRecognizer(tapRec)
-        tapRec.cancelsTouchesInView = false
+        self.sceneView.scene.physicsWorld.contactDelegate = self
     }
     // trigger when touch the screen
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if self.basketAdded == true {
-            timer.perform(closure: {()-> NextStep in
-                self.power = self.power + 1
-                return .continue
-            })
-        }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if self.basketAdded == true{
-            self.timer.stop()
-            self.shootBall()
-        }
-        self.power = 1
-    }
-    
-    
-    
-    func shootBall(){
-        guard let pointOfView = self.sceneView.pointOfView else { return }
-        self.removeOtherBall()
-        let transform = pointOfView.transform
-        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
-        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
-        let position = location + orientation
-        let ball = SCNNode(geometry: SCNSphere(radius: 0.2))
-        ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "ball")
-        ball.position = position
-        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
-        ball.physicsBody = body
-        ball.name = "Basketball"
-        // control power when ball hit the basket
-        body.restitution = 0.2
-        ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
         
-        self.sceneView.scene.rootNode.addChildNode(ball)
     }
+    
+    
+    @IBAction func addTargets(_ sender: Any) {
+        self.addEgg(x: 5, y: 0, z: -40)
+        self.addEgg(x: 0, y: 0, z: -40)
+        self.addEgg(x: -5, y: 0, z: -40)
+    }
+    
+    func addEgg(x:Float,y:Float,z:Float){
+        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
+        let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+        eggNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: eggNode, options: nil))
+        eggNode.position = SCNVector3(x,y,z)
+        eggNode.physicsBody?.categoryBitMask = BitMaskCategory.target.rawValue
+        eggNode.physicsBody?.contactTestBitMask = BitMaskCategory.bullet.rawValue
+        self.sceneView.scene.rootNode.addChildNode(eggNode)
+    }
+    
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let nodeA = contact.nodeA
+        let nodeB = contact.nodeB
+        
+        if nodeA.physicsBody?.categoryBitMask == BitMaskCategory.target.rawValue {
+            self.Target = nodeA
+        }else if nodeB.physicsBody?.categoryBitMask == BitMaskCategory.target.rawValue {
+            self.Target = nodeB
+        }
+        
+//        let spark = SCNParticleSystem(named: "Media.scnassets/Spark.skn", inDirectory: nil)
+//        spark?.loops = false
+//        spark?.particleLifeSpan = 4
+//        spark?.emitterShape = Target?.geometry
+//        let sparkNode = SCNNode()
+//        sparkNode.addParticleSystem(spark!)
+//        sparkNode.position = contact.contactPoint
+//        self.sceneView.scene.rootNode.addChildNode(sparkNode)
+        Target?.removeFromParentNode()
+    }
+    
     
     @objc func handleTap(sender:UITapGestureRecognizer){
         guard let sceneView = sender.view as? ARSCNView else {
             return
         }
-        let touchLocation = sender.location(in: sceneView)
-        let hitTest = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingExtent])
-        if !hitTest.isEmpty {
-            self.addBasket(hitTestResult:hitTest.first!)
-        }
+        let pointOfView = sceneView.pointOfView!
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let position = orientation + location
+        let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        bullet.position = position
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet))
+        body.isAffectedByGravity = false
+        bullet.physicsBody = body
+        bullet.physicsBody?.applyForce(SCNVector3(orientation.x*power,orientation.y*power,orientation.z*power), asImpulse: true)
+        bullet.physicsBody?.categoryBitMask = BitMaskCategory.bullet.rawValue
+        bullet.physicsBody?.contactTestBitMask = BitMaskCategory.target.rawValue
+        self.sceneView.scene.rootNode.addChildNode(bullet)
+        bullet.runAction(SCNAction.sequence([
+            SCNAction.wait(duration: 2.0),
+            SCNAction.removeFromParentNode()
+        ]))
     }
     
-    func addBasket(hitTestResult:ARHitTestResult){
-        if basketAdded == false {
-            let backetScene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
-            let basketNode = backetScene?.rootNode.childNode(withName: "Basket", recursively: false)
-            let positionOfPlane = hitTestResult.worldTransform.columns.3
-            let xPosition = positionOfPlane.x
-            let yPosition = positionOfPlane.y
-            let zPosition = positionOfPlane.z
-            basketNode?.position = SCNVector3(xPosition,yPosition,zPosition)
-            // have body but not affected with other forces like gravity
-            basketNode?.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: basketNode!, options: [
-                // treat box and torus as seprate shapes
-                SCNPhysicsShape.Option.keepAsCompound:true,
-                // high level of detaisl
-                SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron
-            ]))
-            self.sceneView.scene.rootNode.addChildNode(basketNode!)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
-                self.basketAdded = true
-            }
-        }
-    }
     
-    deinit {
-        self.timer.stop()
-    }
     
-    func removeOtherBall(){
-        self.sceneView.scene.rootNode.enumerateChildNodes{(node,_) in
-            if node.name == "Basketball"{
-                node.removeFromParentNode()
-            }
-        }
-        
-        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-            guard anchor is ARPlaneAnchor else {
-                return
-            }
-            DispatchQueue.main.async {
-                self.planeDetected.isHidden = false
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3){
-                self.planeDetected.isHidden = true
-            }
-        }
-        
-        
-        
-    }
 }
 
 func +(left:SCNVector3,right:SCNVector3)->SCNVector3{
